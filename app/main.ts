c5780539132858c455231ae6730f3711c7afdc84
Bijay Khapung <khapungbj84@gmail.com>
2024-07-17 15:39:32 +0545

    Read user-agent

--- app/main.ts
+++ app/main.ts
@@ -17,6 +17,16 @@ const server = net.createServer((socket: net.Socket) => {
       const userString = split[split.length - 1];
       const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userString.length}\r\n\r\n${userString}`;
       socket.write(response);
+    } else if (url == "/user-agent" || url == "/user-agent/") {
+      const lowerCaseDecodedData = decodedData.toLowerCase();
+      const indexOfUserAgent = lowerCaseDecodedData.indexOf("user-agent:");
+      const userAgent = decodedData
+        .slice(indexOfUserAgent)
+        .split("\r\n")[0]
+        .split(":")[1]
+        .trim();
+      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+      socket.write(response);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
