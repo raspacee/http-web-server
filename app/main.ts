3e0bad7ad469d6bc81484b66c84d2b7991b0e2f1
Bijay Khapung <khapungbj84@gmail.com>
2024-07-17 14:52:35 +0545

    Respond with 200

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,10 @@
 import * as net from "net";
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+  // socket.on("close", () => {
+  socket.end();
+  // });
 });
 
 server.listen(4221, "localhost");
