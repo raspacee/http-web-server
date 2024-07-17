be3227817b2dd87d52b116cd7867e88ca418d428
Bijay Khapung <khapungbj84@gmail.com>
2024-07-17 15:02:49 +0545

    Extract URL path

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,25 @@
 import * as net from "net";
 
-const server = net.createServer((socket) => {
-  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
-  // socket.on("close", () => {
-  socket.end();
-  // });
+const parseUrl = (request: string): string => {
+  const texts = request.split(" ");
+  const url = texts[1];
+  return url;
+};
+
+const server = net.createServer((socket: net.Socket) => {
+  socket.on("data", (data) => {
+    const decodedData = data.toString();
+    const url = parseUrl(decodedData);
+    if (url == "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  });
+
+  socket.on("close", () => {
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
